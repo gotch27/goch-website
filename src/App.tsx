5032ae2e2709994a92143285aa57d8f117373427
Gorazd <fi.gorazd@gmail.com>
2025-12-15 23:07:21 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import gochLogo from "./assets/goch.svg";
+import { SocialLinks } from "./components/SocialLinks";
+
+function App() {
+  return (
+    <main className="container">
+      <div className="content">
+        <img src={gochLogo} alt="Goch Logo" className="logo" />
+        <SocialLinks />
+        <div className="construction">
+          <span className="construction-icon">🚧</span>
+          <p>This website is under construction</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
